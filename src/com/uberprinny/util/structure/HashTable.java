275b919dc7b2b4ce34f7710fac2ca71f012b5bb5
Eric Brewer <eric.r.brewer@gmail.com>
2016-02-02 14:53:31 -0800

    Initial commit.
    Implementation of a map which uses a hash function.

---
+++ src/com/uberprinny/util/structure/HashTable.java
@@ -0,0 +1,187 @@
+package com.uberprinny.util.structure;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+public class HashTable<K, V> implements Map<K, V> {
+	
+	private class Entry<K2 extends K, V2 extends V> implements Map.Entry<K, V> {
+		
+		Entry(K2 key, V2 value) {
+			mKey = key;
+			mValue = value;
+		}
+		
+		private K2 mKey;
+		
+		private V mValue;
+
+		@Override
+		public K getKey() {
+			return mKey;
+		}
+
+		@Override
+		public V getValue() {
+			return mValue;
+		}
+
+		@Override
+		public V setValue(V value) {
+			V val = mValue;
+			mValue = value;
+			return val;
+		}
+	}
+	
+	public HashTable() {
+		@SuppressWarnings("unchecked")
+		LinkedList<Entry<K, V>>[] a = (LinkedList<Entry<K, V>>[]) new LinkedList[HASH];
+		mTray = a;
+	}
+	
+	private static final int HASH = 107; // prime
+	
+	private LinkedList<Entry<K, V>>[] mTray;
+	
+	// TODO Add V mNullKey;, modify getSize()
+	
+	private int mSize = 0;
+	
+	private LinkedList<Entry<K, V>> getTray(Object key) {
+		int code = key.hashCode()%HASH;
+		if (mTray[code] == null) {
+			mTray[code] = new LinkedList<Entry<K, V>>();
+		}
+		return mTray[code];
+	}
+	
+	@Override
+	public void clear() {
+		for (int i = 0; i < HASH; i++) {
+			mTray[i] = null;
+		}
+		mSize = 0;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		for (Entry<K, V> entry : getTray(key)) {
+			if (entry.getKey().equals(key)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		for (LinkedList<Entry<K, V>> tray : mTray) {
+			if (tray != null) {
+				for (Entry<K, V> entry : tray) {
+					if (entry.getValue().equals(value)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Set<Map.Entry<K, V>> entrySet() {
+		HashSet<Map.Entry<K, V>> s = new HashSet<Map.Entry<K, V>>();
+		for (LinkedList<Entry<K, V>> tray : mTray) {
+			if (tray != null) {
+				for (Entry<K, V> entry : tray) {
+					s.add(entry);
+				}
+			}
+		}
+		return s;
+	}
+
+	@Override
+	public V get(Object key) {
+		for (Entry<K, V> entry : getTray(key)) {
+			if (entry.getKey().equals(key)) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return mSize == 0;
+	}
+
+	@Override
+	public Set<K> keySet() {
+		HashSet<K> s = new HashSet<K>();
+		for (LinkedList<Entry<K, V>> tray : mTray) {
+			if (tray != null) {
+				for (Entry<K, V> entry : tray) {
+					s.add(entry.getKey());
+				}
+			}
+		}
+		return s;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		LinkedList<Entry<K, V>> tray = getTray(key);
+		for (Entry<K, V> entry : tray) {
+			if (entry.getKey().equals(key)) { // maintains uniqueness of keys
+				return entry.setValue(value);
+			}
+		}
+		tray.push(new Entry<K, V>(key, value));
+		mSize++;
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		for (K key : m.keySet()) {
+			put(key, m.get(key));
+		}
+	}
+
+	@Override
+	public V remove(Object key) {
+		LinkedList<Entry<K, V>> tray = getTray(key);
+		for (int i = 0; i < tray.size(); i++) {
+			Entry<K, V> entry = tray.get(i);
+			if (entry.getKey().equals(key)) {
+				tray.remove(i);
+				mSize--;
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return mSize;
+	}
+
+	@Override
+	public Collection<V> values() {
+		ArrayList<V> values = new ArrayList<V>();
+		for (LinkedList<Entry<K, V>> tray : mTray) {
+			if (tray != null) {
+				for (Entry<K, V> entry : tray) {
+					values.add(entry.getValue());
+				}
+			}
+		}
+		return values;
+	}
+}
